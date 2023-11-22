9158e21d4bafba536a650990eca640948571bc38
endoscopysubennj <139906984+endoscopysubennj@users.noreply.github.com>
2023-11-22 10:15:10 -0600

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,52 +1,27 @@
-def main():
-    main()
 def generate_random_data():
+
     return data
-        print(f"Random Number: {item}")
-import random
-    for item in data:
 
     data = [random.randint(1, 100) for _ in range(10)]
 
-
-    data = generate_random_data()
-if __name__ == "__main__":
-    main()
-
-        print(f"Random Number: {item}")
-
-def generate_random_data():
-    return data
-def main():
-if __name__ == "__main__":
-    data = [random.randint(1, 100) for _ in range(10)]
-
-    data = generate_random_data()
-
 import random
-    for item in data:    data = [random.randint(1, 100) for _ in range(10)]
-        print(f"Random Number: {item}")
-
-def generate_random_data():
-def main():
-    data = generate_random_data()
-    main()
+if __name__ == "__main__":
     for item in data:
-
-import random
-    return data
-
-
-if __name__ == "__main__":    return data
         print(f"Random Number: {item}")
-
-    main()
-    data = [random.randint(1, 100) for _ in range(10)]
-
-    for item in data:
-
     data = generate_random_data()
-import random
-def generate_random_data():
-if __name__ == "__main__":
+    main()
 def main():
+def generate_random_data():
+
+        print(f"Random Number: {item}")
+    for item in data:
+    return data
+import random
+    data = [random.randint(1, 100) for _ in range(10)]
+    main()
+
+
+def main():
+
+if __name__ == "__main__":
+    data = generate_random_data()
